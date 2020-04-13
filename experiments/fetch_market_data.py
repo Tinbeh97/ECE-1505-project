8137de94787be3868b6b80929ced8bf136e7f723
Yuan Liu <yuan6liu@gmail.com>
2020-04-12 21:43:26 -0700

    edit: script to support fetch data for different intervals

--- experiments/fetch_market_data.py
+++ experiments/fetch_market_data.py
@@ -9,15 +9,20 @@ import matplotlib.pyplot as plt
 import json
 import sys
 
-tickers_0 = {
-    "test":
-    ['aapl','goog','cvx','sne','regn','amd','atvi','aptv','tif','bidu']
-}
+# tickers_0 = {
+#     "name": "test",
+#     "date_window": ["2014-02-01", "2014-02-01"],
+#     "interval": "1d",
+#     "tickers":
+#     ['aapl','goog','cvx','sne','regn','amd','atvi','aptv','tif','bidu']
+# }
 
-def  get_data_dim(ticker, time_start, time_end):
+def  get_data_dim(ticker, time_start, time_end, interval):
     tk = yf.Ticker(ticker)
-    history = tk.history(# period=period,
-                         start=time_start, end=time_end)
+
+    history = tk.history(start=time_start,
+                         end=time_end,
+                         interval=interval)
     print("history dict: " + str(history.__dict__)) #see data format here
     df = pd.DataFrame(history)
     # print(df)
@@ -26,10 +31,11 @@ def  get_data_dim(ticker, time_start, time_end):
     print("ticker: " + ticker + ", data shape: " + str(arr.shape))
     return arr.shape
 
-def get_data(ticker, time_start, time_end):
-    ticker = yf.Ticker(ticker)
-    history = ticker.history(# period=period,
-                             start=time_start, end=time_end)
+def get_data(ticker, time_start, time_end, interval):
+    tk = yf.Ticker(ticker)
+    history = tk.history(start=time_start,
+                             end=time_end,
+                             interval=interval)
     df = pd.DataFrame(history)
     arr = df.to_numpy()
     return arr
@@ -54,21 +60,21 @@ def plot_data(tickers, arr, time_unit, val_type, is_log_scale=True):
     ax.legend(loc='upper left', bbox_to_anchor=(1.005, 1.0), shadow=True, ncol=4)
     plt.show()
 
-def fetch(tickers, time_window):
+def fetch(tickers, time_window, interval):
 
     #give a dummy stock which has long enough history for the time_window to query for data dimension size
     
-    dims_expected = get_data_dim('aapl', *time_window)  #get daily prices within time_window
+    dims_expected = get_data_dim('msft', *time_window, interval)  #get daily prices within time_window
 
     #filter out tickers that don't have expected dimension from query
     def filt_shape(item):
         (idx, data) = item
         print("filtering: " + tickers[idx])
-        if data.shape != dims_expected:
+        if data.shape[0] < dims_expected[0]:
             print("discard: " + tickers[idx] + ", shape: " + str(data.shape), ", expected: " + str(dims_expected))
         return data.shape == dims_expected
 
-    tickers_data = enumerate(map(lambda x: get_data(x, *time_window), tickers))
+    tickers_data = enumerate(map(lambda x: get_data(x, *time_window, interval), tickers))
 
     tickers_data_filt = list(filter(filt_shape, tickers_data))
 
@@ -76,7 +82,7 @@ def fetch(tickers, time_window):
 
     tickers_filt_name = list(map(lambda idx: tickers[idx], tickers_filt_idx))
     
-    arr = np.stack(data)
+    arr = np.stack(data)[0:dims_expected[0],...]
 
     return tickers_filt_idx, tickers_filt_name, arr
 
@@ -95,13 +101,17 @@ if __name__ == "__main__":
     assert(data is not None)
     
     #fetch
-    (data_name, tickers, time_window) = (data["name"], data["tickers"], data["date_window"])
+    (data_name, tickers, time_window, interval) = (data["name"],
+                                                   data["tickers"],
+                                                   data["date_window"],
+                                                   data["interval"])
     print("data name: ", data_name)
     print("tickers: ", tickers)
     print("time_window: ", str(time_window))
+    print("interval: ", interval)
     
     # time_window = ["2014-01-01", "2018-12-31"]
-    tk_idx, tk_name, tk_arr = fetch(tickers, time_window)
+    tk_idx, tk_name, tk_arr = fetch(tickers, time_window, interval)
 
     #save ticker data and filtered ticker list
     record_data = 'fetched/'+data_name+'_'+time_window[0]+'_'+time_window[1]+'.npy'
@@ -111,13 +121,30 @@ if __name__ == "__main__":
 
     #check saved data
     d = np.load(record_data)
-    assert(np.all(tk_arr == d))
+    # assert(np.all(tk_arr == d))
     d = np.loadtxt(record_tickers, dtype='str')
-    assert(all(map(lambda x: x[0] == x[1], zip(d,tk_name))))
+    # assert(all(map(lambda x: x[0] == x[1], zip(d,tk_name))))
 
     #test plot
-    plot_data(tk_name, tk_arr[:,:,0], "day", "opening price", is_log_scale=True)
-    plot_data(tk_name, tk_arr[:,:,3], "day", "closing price", is_log_scale=True)
-    plot_data(tk_name, tk_arr[:,:,4], "day", "volume", is_log_scale=False)
+    x_label = "unknown"
+    index_day = interval.find("d")
+    if index_day != -1:
+        x_label = "days("+interval[0:index_day]+")"
+        
+    index_min = interval.find("m")
+    if index_min != -1 and interval.find("mo") == -1:
+        x_label = "minutes("+interval[0:index_min]+")"
+
+    index_month = interval.find("mo")
+    if index_month != -1:
+        x_label = "months("+interval[0:index_month]+")"
+
+    index_year = interval.find("y")
+    if index_year != -1:
+        x_label = "years("+interval[0:index_year]+")"
+        
+    plot_data(tk_name, tk_arr[:,:,0], x_label, "opening price", is_log_scale=True)
+    plot_data(tk_name, tk_arr[:,:,3], x_label, "closing price", is_log_scale=True)
+    plot_data(tk_name, tk_arr[:,:,4], x_label, "volume", is_log_scale=False)
 
 
--- experiments/save_ticker.py
+++ experiments/save_ticker.py
@@ -3,20 +3,31 @@
 
 import json
 
-tickers_sp100_2016 = {
-    "name": "sp100_2016",
+#select some stocks, using s&p100 at april 6,2020, see their history from 2014 to 2018
+tickers_sp100_2014_2018_1d = {
+    "name": "sp100_2014_2018_1d",
     "date_window": ["2014-01-01", "2018-12-31"],
-    "tickers":
-    ['AAPL','ABBV','ABT','ACN','AGN','AIG','ALL','AMGN','AMZN','AXP','BA','BAC','BIIB','BK','BLK','BMY','C','CAT','CELG','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DHR','DIS','DOW','DUK','EMC','EMR','EXC','F','FB','FDX','FOX','FOXA','GD','GE','GILD','GM','GOOG','GOOGL','GS','HAL','HD','HON','IBM','INTC','JNJ','JPM','KMI','KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MON','MRK','MS','MSFT','NEE','NKE','ORCL','OXY','PCLN','PEP','PFE','PG','PM','PYPL','QCOM','RTN','SBUX','SLB','SO','SPG','T','TGT','TWX','TXN','UNH','UNP','UPS','USB','USD','UTX','V','VZ','WBA','WFC']
-}
-
-tickers_sp100_2020_april_6 = {
-    "name": "sp100_2020_april_6",
-    "date_window": ["2014-01-01", "2018-12-31"],
+    "interval": "1d",
     "tickers":
     ['AAPL','ABBV','ABT','ACN','ADBE','AGN','AIG','ALL','AMGN','AMT','AMZN','AXP','BA','BAC','BIIB','BK','BKNG','BLK','BMY','BRK-B','C','CAT','CHTR','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DHR','DIS','DUK','EMR','EXC','F','FB','FDX','GD','GE','GILD','GM','GOOG','GOOGL','GS','HD','HON','IBM','INTC','JNJ','JPM','KMI','KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MRK','MS','MSFT','NEE','NFLX','NKE','NVDA','ORCL','OXY','PEP','PFE','PG','PM','PYPL','QCOM','RTX','SBUX','SLB','SO','SPG','T','TGT','TMO','TXN','UNH','UNP','UPS','USB','V','VZ','WBA','WFC','WMT','XOM']}
 
-l = [tickers_sp100_2016, tickers_sp100_2020_april_6]
+#see only 1 day history at 1min intervals
+tickers_sp100_2020_03_25_1m = {
+    "name": "sp100_2020_03_25_1m",
+    "date_window": ["2020-03-25", "2020-03-26"],
+    "interval": "1m",
+    "tickers":
+    ['AAPL','ABBV','ABT','ACN','ADBE','AGN','AIG','ALL','AMGN','AMT','AMZN','AXP','BA','BAC','BIIB','BK','BKNG','BLK','BMY','BRK-B','C','CAT','CHTR','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DHR','DIS','DUK','EMR','EXC','F','FB','FDX','GD','GE','GILD','GM','GOOG','GOOGL','GS','HD','HON','IBM','INTC','JNJ','JPM','KMI','KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MRK','MS','MSFT','NEE','NFLX','NKE','NVDA','ORCL','OXY','PEP','PFE','PG','PM','PYPL','QCOM','RTX','SBUX','SLB','SO','SPG','T','TGT','TMO','TXN','UNH','UNP','UPS','USB','V','VZ','WBA','WFC','WMT','XOM']}
+
+#see only 1 day history at 2min intervals
+tickers_sp100_2020_03_25_2m = {
+    "name": "sp100_2020_03_25_2m",
+    "date_window": ["2020-03-25", "2020-03-26"],
+    "interval": "2m",
+    "tickers":
+    ['AAPL','ABBV','ABT','ACN','ADBE','AGN','AIG','ALL','AMGN','AMT','AMZN','AXP','BA','BAC','BIIB','BK','BKNG','BLK','BMY','BRK-B','C','CAT','CHTR','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DHR','DIS','DUK','EMR','EXC','F','FB','FDX','GD','GE','GILD','GM','GOOG','GOOGL','GS','HD','HON','IBM','INTC','JNJ','JPM','KMI','KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MRK','MS','MSFT','NEE','NFLX','NKE','NVDA','ORCL','OXY','PEP','PFE','PG','PM','PYPL','QCOM','RTX','SBUX','SLB','SO','SPG','T','TGT','TMO','TXN','UNH','UNP','UPS','USB','V','VZ','WBA','WFC','WMT','XOM']}
+
+l = [tickers_sp100_2014_2018_1d, tickers_sp100_2020_03_25_1m, tickers_sp100_2020_03_25_2m]
 
 for i in l:
     
